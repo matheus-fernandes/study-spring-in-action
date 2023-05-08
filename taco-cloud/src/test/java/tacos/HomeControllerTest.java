867440d342cccc27e9a651604e2fb4a8402b5f60
Matheus Fernandes <matheusfsilva10@gmail.com>
2023-05-08 20:39:46 -0300

    fixing HomeControllerTest configuration beans

--- taco-cloud/src/test/java/tacos/HomeControllerTest.java
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -4,9 +4,14 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import tacos.constant.ProfileConstants;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
--- taco-cloud/src/test/java/tacos/WebMvcConfiguration.java
+++ taco-cloud/src/test/java/tacos/WebMvcConfiguration.java
@@ -1,22 +1,65 @@
 package tacos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import tacos.repository.IngredientRepository;
-import tacos.repository.OrderRepository;
+import tacos.repository.UserRepository;
+import tacos.security.HttpSecurityDecorator;
+import tacos.security.HttpSecurityDecoratorDefault;
+import tacos.security.SecurityConfig;
+import tacos.service.OrderService;
+import tacos.service.TacoService;
 
 @TestConfiguration
 public class WebMvcConfiguration {
     @MockBean
+    TacoService tacoService;
+
+    @MockBean
     IngredientRepository ingredientRepository;
 
     @MockBean
-    OrderRepository orderRepository;
+    PasswordEncoder passwordEncoder;
+
+    @MockBean
+    OrderService orderService;
+
+    @MockBean
+    UserRepository userRepository;
 
     @Bean
-    OrderRepository getOrderRepository(){
-        return orderRepository;
+    SecurityFilterChain getHttpSecurityConfig(HttpSecurity httpSecurity, HttpSecurityDecorator decorator) throws Exception {
+        return new SecurityConfig().securityFilterChain(httpSecurity, decorator);
+    }
+
+    @Bean
+    HttpSecurityDecorator getHttpSecurityDecorator(){
+        return new HttpSecurityDecoratorDefault();
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return passwordEncoder;
+    }
+
+    @Bean
+    UserRepository getUserRepository(){
+        return userRepository;
+    }
+
+    @Bean
+    TacoService getTacoService(){
+        return tacoService;
+    }
+
+    @Bean
+    OrderService getOrderService(){
+        return orderService;
     }
 
     @Bean
